90768c4f7a45fc061713cfc13d4d9559325c0e53
iTeV <iTeV@fs0ciety.xyz>
2017-12-09 23:59:41 +0100

    Added connectivity check

--- src/yeelight.php
+++ src/yeelight.php
@@ -2,14 +2,29 @@
 
 class Yeelight{
 
+		protected $bulbIP;
+		protected $bulbPort;
+		protected $connErrNum;
+		protected $connErrMsg;
+
 		public function __construct($bulbIP, $bulbPort){
-				if(!$this->testConnection($bulbIP, $bulbPort)){
-					throw new Exception("Could not connect to the bulb!");
+				$this->bulbIP = $bulbIP;
+				$this->bulbPort = $bulbPort;
+				if(!$this->testConnection($this->bulbIP, $this->bulbPort)){
+					throw new Exception("Could not connect to the bulb, error ". $this->connErrNum. " : ".$this->connErrMsg );
 				} else {
-						$this->bulbIP = $bulbIP;
-						$this->bulbPort = $bulbPort;
+					echo "Successfully established a connection to ".$this->bulbIP;
+
+
 				}
 
+		}
+
+		public function testConnection(){
+				if(fsockopen($this->bulbIP, $this->bulbPort, $this->connErrNum, $this->connErrMsg)){
+						return true;
+				}
+		
 		}
 
 
