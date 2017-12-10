4e443b90663ba0c7bd38c23a02af5638016c87c5
iTeV <iTeV@fs0ciety.xyz>
2017-12-10 20:05:19 +0100

    Changed the way how the array is saved

--- src/detectYeelight.php
+++ src/detectYeelight.php
@@ -8,18 +8,20 @@ class detectYeelight{
 		protected $socket;
 		protected $socketSendResponse;
 		protected $tmp = null;
+		protected $resultArray;
 
 		public function searchDevices(){
 				$this->socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 				socket_set_option($this->socket, SOL_SOCKET, SO_RCVTIMEO, array('sec'=> 15, 'usec'=>10000));
-				var_dump($this->socketSendResponse = socket_sendto($this->socket, $this->headers, 1024, 0, '239.255.255.250', '1982'));
+				$this->socketSendResponse = socket_sendto($this->socket, $this->headers, 1024, 0, '239.255.255.250', '1982');
 
 				# Wait for response
 				while(@socket_recvfrom($this->socket, $this->buff, 1024, MSG_WAITALL, $this->tmp, $this->tmp)){
 						$listtest = list($status, $St, $Location, $Opt, $N1s, $Server) = explode("\r\n", $this->buff);
-						var_dump($listtest);
+						$this->resultArray = $listtest;
 						        }
-		socket_close($this->socket);
+				socket_close($this->socket);
+				print_r($this->resultArray);
 		}
 
 
